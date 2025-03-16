63450c0c45363e77d7981f205d527ebc41352ff1
Sehez Babber <sehez3010@gmail.com>
2025-03-17 00:25:02 +0530

    about done

--- Frontend/src/landingpage/about/Hero.jsx
+++ Frontend/src/landingpage/about/Hero.jsx
@@ -1,7 +1,68 @@
 function Hero() {
-    return ( 
-        <h1>Hero_About</h1>
-     );
+  return (
+    <div
+      className="container d-flex flex-column justify-content-center align-items-center"
+      style={{ marginTop: "5vw" }}
+    >
+      <div
+        className="d-flex flex-column justify-content-center align-items-center text-center"
+        style={{ marginBottom: "5vw" }}
+      >
+        <h1 className="fs-2">
+          We pioneered the discount broking model in India.
+        </h1>
+        <h1 className="fs-2">
+          Now, we are breaking ground with our technology.
+        </h1>
+      </div>
+      <hr style={{ width: "70vw", marginBottom: "7vw" }} />
+      <div
+        className="d-flex flex-row justify-content-between align-items-start"
+        style={{ width: "60vw" }}
+      >
+        {/* Ensure both divs take equal width */}
+        <div className="d-flex flex-column justify-content-center align-items-start" style={{width:"40%"}}>
+          <p>
+            We kick-started operations on the 15th of August, 2010 with the goal
+            of breaking all barriers that traders and investors face in India in
+            terms of cost, support, and technology. We named the company
+            Zerodha, a combination of Zero and "Rodha", the Sanskrit word for
+            barrier.
+          </p>
+          <br />
+          <p>
+            Today, our disruptive pricing models and in-house technology have
+            made us the biggest stock broker in India.
+          </p>
+          <br />
+          <p>
+            Over 1+ Crore clients place millions of orders every day through our
+            powerful ecosystem of investment platforms, contributing over 15% of
+            all Indian retail trading volumes.
+          </p>
+        </div>
+
+        <div className="d-flex flex-column justify-content-center align-items-start w-50" style={{width:"40%"}}>
+          <p>
+            In addition, we run a number of popular open online educational and
+            community initiatives to empower retail traders and investors.
+          </p>
+          <br />
+          <p>
+            Rainmatter, our fintech fund and incubator, has invested in several
+            fintech startups with the goal of growing the Indian capital
+            markets.
+          </p>
+          <br />
+          <p>
+            And yet, we are always up to something new every day. Catch up on
+            the latest updates on our blog or see what the media is saying about
+            us.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- Frontend/src/landingpage/about/Team.jsx
+++ Frontend/src/landingpage/about/Team.jsx
@@ -1,6 +1,22 @@
 function Team() {
     return ( 
-        <h1>Team</h1>
+        <div className="container">
+        <div className="container d-flex justify-content-center align-items-center" style={{width:"100vw",marginBottom:"10vw"}}>
+            <h1>People</h1>
+        </div>
+        <div className="container d-flex justify-content-center align-items-start flex-row">
+            <div className="img_div d-flex justify-content-center align-items-center flex-column" style={{width:"40%"}}>
+                <img src="\media\images\nithinKamath.jpg" alt="ceo_img" className="rounded-circle" style={{width:"60%"}}/>
+                <h3>Nithin Kamath</h3>
+                <p>Founder, CEO</p>
+            </div>
+            <div className="container d-flex justify-content-center align-items-start flex-column m-0" style={{width:"40%"}}>
+                <p>Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry.</p>
+                <p>He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).</p>
+                <p>Playing basketball is his zen.</p>
+            </div>
+        </div>
+        </div>
      );
 }
 
